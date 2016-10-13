4a9d5ea128f114b412597cdf21aaf456a4fe0dc4
raku-cat <raku@rakutiki.tv>
2016-10-13 10:28:19 -0500

    initial

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,4 +1,3 @@
 #!/system/bin/sh
 
-# This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
+/magisk/.core/bin/sepolicy-inject --live -s mediaserver -t mediaserver_tmpfs -c file -p read,write,execute
